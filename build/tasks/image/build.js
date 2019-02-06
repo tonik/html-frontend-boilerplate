058832d4b611a574a06674e8f42054f91a770fdd
Klaudia <klaudia@tonik.pl>
2019-02-06 15:49:46 +0100

    Use imagemin-mozjpeg plugin for better jpeg compression

--- build/tasks/image/build.js
+++ build/tasks/image/build.js
@@ -3,12 +3,15 @@ const isdev = require('isdev')
 const gulp = require('gulp')
 const gulpif = require('gulp-if')
 const imagemin = require('gulp-imagemin')
+const imageminMozjpeg = require('imagemin-mozjpeg')
 
 const message = require('../../lib/message')
 
 module.exports = () => {
   return gulp.src('../resources/assets/images/**/*.{jpg,jpeg,png,gif,svg}')
-    .pipe(gulpif(!isdev, imagemin()))
+    .pipe(gulpif(!isdev, imagemin([
+      imageminMozjpeg({quality: 74})
+    ])))
     .on('error', message.error('IMAGE: Minification'))
     .pipe(gulp.dest('../public/images'))
 }
