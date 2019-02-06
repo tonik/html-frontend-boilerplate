446bb6f5ff95165711070bfd45f3b0db6027e5ee
Klaudia <klaudia@tonik.pl>
2019-02-06 16:30:32 +0100

    Increase png compression

--- build/tasks/image/build.js
+++ build/tasks/image/build.js
@@ -4,14 +4,18 @@ const gulp = require('gulp')
 const gulpif = require('gulp-if')
 const imagemin = require('gulp-imagemin')
 const imageminMozjpeg = require('imagemin-mozjpeg')
+const imageminPngquant = require('imagemin-pngquant')
 
 const message = require('../../lib/message')
 
 module.exports = () => {
   return gulp.src('../resources/assets/images/**/*.{jpg,jpeg,png,gif,svg}')
     .pipe(gulpif(!isdev, imagemin([
-      imageminMozjpeg({quality: 74})
-    ])))
+      imageminMozjpeg({quality: 74}),
+      imageminPngquant()
+    ], {
+      verbose: true
+    })))
     .on('error', message.error('IMAGE: Minification'))
     .pipe(gulp.dest('../public/images'))
 }
