2effa8849b23cb97c5365e3ea1ab2a738ed617b5
jedrzejchalubek <jedrzej.chalubek@gmail.com>
2019-07-15 16:04:50 +0200

    feat(): Realase version 2.0
    * Update stylelint to newest versions
    
    * Introduce SCSS autofixing

---
+++ build/tasks/sass/fix.js
@@ -0,0 +1,15 @@
+const gulp = require('gulp')
+const stylelint = require('gulp-stylelint')
+
+const message = require('../../lib/message')
+
+module.exports = () => {
+  return gulp.src('../resources/assets/sass/**/*.scss')
+    .pipe(stylelint({
+      fix: true,
+      reporters: [{ formatter: 'string', console: true }]
+    }))
+
+    .on('error', message.error('SASS: Fixing'))
+    .pipe(gulp.dest('../resources/assets/sass'))
+}
